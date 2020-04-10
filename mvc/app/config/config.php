b6ed6b2af67c8212c39b1c39fdc27eacfe55db58
Simon Taylor <simon.taylor@tyxan.com>
2020-04-10 20:17:25 +0100

    Global config file using constants

---
+++ mvc/app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+  // App root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL root
+  define('URLROOT', 'http://localhost/udemy-object-oriented-php-mvc/mvc');
+  // Site name
+  define('SITENAME', 'PHP-MVC');

1fea8b3635ef2bd4c1c8f24037fa5c3daa380093
Simon Taylor <simon.taylor@tyxan.com>
2020-04-10 18:39:06 +0100

    Setup core class. Example method getURL grabs query string

--- mvc/app/libraries/core.php
+++ mvc/app/libraries/core.php
@@ -5,3 +5,23 @@
     * As the name suggests, it sits at the core of our project, bringing
     * all our different files together.
     */
+
+  /**
+    * App core class
+    * Creates URL and core controller
+    * URL format - /controller/method/params
+    */
+  class Core {
+    // Properties
+    protected $currentController = "Pages";
+    protected $currentMethod = "Index";
+    protected $pararms = [];
+    // Methods
+    public static function getURL() {
+      echo $_GET['url'] ?: '';
+    }
+    // Construct
+    public function __construct() {
+      $this->getURL;
+    }
+  }

a075643a4f1f5cd5fd52a05cc5c0c0448da65225
Simon Taylor <simon.taylor@tyxan.com>
2020-04-10 19:57:56 +0100

    Extends Controller class. Example view added. Little tarting up.

--- mvc/app/controllers/Pages.php
+++ mvc/app/controllers/Pages.php
@@ -1,15 +1,15 @@
 <?php
-  class Pages {
+  class Pages extends Controller {
     public function __construct() {
 
     }
 
     // Must have the index function as it's default for /libraries/Core.php
     public function index() {
-
+      $this->view('hello');
     }
 
     public function about($id) {
-      echo "ID: ". $id ." requested";
+      echo "ID: $id requested";
     }
   }

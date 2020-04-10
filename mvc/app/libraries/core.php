1072c503a564954f51dc853a0a471332e88a63ad
Simon Taylor <simon.taylor@tyxan.com>
2020-04-10 19:45:02 +0100

    Added currentMethod using 2nd part of URL query &  params for remainder

--- mvc/app/libraries/core.php
+++ mvc/app/libraries/core.php
@@ -14,7 +14,7 @@
   class Core {
     // -- Properties
     protected $currentController = "Pages";
-    protected $currentMethod = "Index";
+    protected $currentMethod = "index";
     protected $pararms = [];
     // -- Construct
     public function __construct() {
@@ -34,6 +34,25 @@
 
       // Instantiate controller class
       $this->currentController = new $this->currentController;
+
+      // Check the requested app controller for the requested method (second part of URL)
+      if(isset($url[1])) :
+        // Check to see if method exists in controller
+        if(method_exists($this->currentController, $url[1])) :
+          $this->currentMethod = $url[1];
+          // Unset 1 index of URL array
+          unset($url[1]);
+        endif;
+      endif;
+
+      // Get params from the URL
+      $this->params = $url ? array_values($url) : [];
+
+      // Call a callback with array of params
+      call_user_func_array([
+        $this->currentController,
+        $this->currentMethod
+      ], $this->params);
     }
     // -- Methods
     public static function getURL() {

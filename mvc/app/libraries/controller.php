257a80797ef92dd40274d5a3c0a63e39446798f8
Simon Taylor <simon.taylor@tyxan.com>
2020-04-10 19:57:26 +0100

    Added model and view methods to the main controller.

--- mvc/app/libraries/controller.php
+++ mvc/app/libraries/controller.php
@@ -1,4 +1,24 @@
 <?php
   /**
-    * Main controller file, load models and views from child controllers.
+    * Main controller file, load models and views for child controllers.
     */
+    class Controller {
+      // Load model
+      public function model($model) {
+        // Require model file
+        require_once "../app/models/$model.php";
+
+        // Instantiate model
+        return new $model();
+      }
+      // Load view
+      public function view($view, $data = []) {
+        // Check the view file exists
+        if(file_exists("../app/views/$view.php")) :
+          require_once "../app/views/$view.php";
+        else :
+          // View does not exists
+          die("View \"$view\" doesn't exist");
+        endif;
+      }
+    }

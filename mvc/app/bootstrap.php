66a711c1c923b82034899729e81b55d24b040449
Simon Taylor <simon.taylor@tyxan.com>
2020-04-10 18:37:58 +0100

    Include our class files from the library directory

--- mvc/app/bootstrap.php
+++ mvc/app/bootstrap.php
@@ -2,3 +2,8 @@
   /**
     * Require all necessary files (libraries, config, helpers, etc.)
     */
+
+    // Load libraries
+    require_once 'libraries/Core.php';
+    require_once 'libraries/Controller.php';
+    require_once 'libraries/Database.php';

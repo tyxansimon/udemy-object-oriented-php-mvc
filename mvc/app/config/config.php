985141556f74672bfbceffa9fee56bdadec1337c
Simon Taylor <simon.taylor@tyxan.com>
2020-04-12 18:14:49 +0100

    Added database params. Existing set params defaulted.

--- mvc/app/config/config.php
+++ mvc/app/config/config.php
@@ -1,7 +1,12 @@
 <?php
+  // DB params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', '_YOUR_USER_');
+  define('DB_PASS', '_YOUR_PASS_');
+  define('DB_NAME', '_DATABASE_NAME_');
   // App root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL root
-  define('URLROOT', 'http://localhost/udemy-object-oriented-php-mvc/mvc');
+  define('URLROOT', '_YOUR_URL_');
   // Site name
-  define('SITENAME', 'PHP-MVC');
+  define('SITENAME', '_YOUR_SITENAME_');

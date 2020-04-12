92bd28df2e85f3ee2268fec2d97e95614ca2e61e
Simon Taylor <simon.taylor@tyxan.com>
2020-04-12 18:15:26 +0100

    Comments for sections of pages controller.

--- mvc/app/controllers/Pages.php
+++ mvc/app/controllers/Pages.php
@@ -1,14 +1,15 @@
 <?php
   class Pages extends Controller {
     public function __construct() {
-
     }
 
     // Must have the index function as it's default for /libraries/Core.php
     public function index() {
+      // View data
       $data = [
         'title' => 'Welcome'
       ];
+      // View template
       $this->view('pages/index', $data);
     }
 

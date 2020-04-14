636e10a1d3b5ba16580cfcf527ca5d38eb0faeb9
Simon Taylor <simon.taylor@tyxan.com>
2020-04-14 12:56:31 +0100

    Include new url helper file

--- mvc/app/bootstrap.php
+++ mvc/app/bootstrap.php
@@ -5,6 +5,8 @@
 
     // Load config
     require_once 'config/config.php';
+    // Load helpers
+    require_once 'helpers/url_helper.php';
 
     // Autoload core libraries
     spl_autoload_register(function($className){

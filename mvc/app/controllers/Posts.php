0961ada90ad01d5c86d16ff4a4164a57ea04f1ae
Simon Taylor <simon.taylor@tyxan.com>
2020-04-14 14:50:35 +0100

    Restricted to logged in users only

--- mvc/app/controllers/Posts.php
+++ mvc/app/controllers/Posts.php
@@ -1,5 +1,10 @@
 <?php
   class Posts extends Controller {
+    public function __construct() {
+      if(!isLoggedIn()) :
+        redirect('users/login');
+      endif;
+    }
 
     public function index() {
       // View data

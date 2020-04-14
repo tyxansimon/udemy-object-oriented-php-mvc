ef3e5c8313d442674a41871c178b9bf282ccc5e4
Simon Taylor <simon.taylor@tyxan.com>
2020-04-14 17:50:36 +0100

    Loop through posts for the post index view

--- mvc/app/views/posts/index.php
+++ mvc/app/views/posts/index.php
@@ -1,5 +1,5 @@
 <?php require APPROOT .'/views/inc/header.php'; ?>
-  <div class="row">
+  <div class="row mb-3">
     <div class="col-md-6">
       <h1><?= $data['title']; ?></h1>
     </div>
@@ -9,4 +9,20 @@
       </a>
     </div>
   </div>
+  <?php
+    foreach($data['posts'] as $post) :
+  ?>
+      <div class="card card-body mb-3">
+        <h4 class="card-title"><?= $post->title ?></h4>
+        <div class="bg-light p-2 mb-3">
+          Written by <?= $post->name ?> on <?= $post->postCreated ?>
+        </div>
+        <p class="card-text"><?= $post->body ?></p>
+        <a href="<?= URLROOT ?>/posts/show/<?= $post->postId ?>" class="btn btn-dark">
+          More
+        </a>
+      </div>
+  <?php
+    endforeach;
+  ?>
 <?php require APPROOT .'/views/inc/footer.php'; ?>

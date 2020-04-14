5ca28d4b444d8f609e305447efc2be1bbb3684cc
Simon Taylor <simon.taylor@tyxan.com>
2020-04-14 11:08:30 +0100

    Intro jumbotron + new description data

--- mvc/app/views/pages/index.php
+++ mvc/app/views/pages/index.php
@@ -1,3 +1,8 @@
 <?php require APPROOT .'/views/inc/header.php'; ?>
-<h1><?= $data['title']; ?></h1>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-3"><?= $data['title']; ?></h1>
+      <p class="lead"><?= $data['description']; ?></p>
+    </div>
+  </div>
 <?php require APPROOT .'/views/inc/footer.php'; ?>

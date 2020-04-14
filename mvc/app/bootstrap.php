f1240b7393486f73bad1add182b6bf8aa593f3b1
Simon Taylor <simon.taylor@tyxan.com>
2020-04-14 13:43:16 +0100

    Include session helper

--- mvc/app/bootstrap.php
+++ mvc/app/bootstrap.php
@@ -7,6 +7,7 @@
     require_once 'config/config.php';
     // Load helpers
     require_once 'helpers/url_helper.php';
+    require_once 'helpers/session_helper.php';
 
     // Autoload core libraries
     spl_autoload_register(function($className){

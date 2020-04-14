1cbecb378f993e477218c6d73f138f74f419f95d
Simon Taylor <simon.taylor@tyxan.com>
2020-04-14 11:08:02 +0100

    Included dependencies - Header got swanky nav

--- mvc/app/views/inc/footer.php
+++ mvc/app/views/inc/footer.php
@@ -1,3 +1,7 @@
+  </main>
+  <script src="<?= URLROOT; ?>/js/jquery.min.js" charset="utf-8"></script>
+  <script src="<?= URLROOT; ?>/js/bootstrap.bundle.min.js" charset="utf-8"></script>
+  <script src="<?= URLROOT; ?>/js/all.min.js" charset="utf-8"></script>
   <script src="<?= URLROOT; ?>/js/scripts.js" charset="utf-8"></script>
 </body>
 </html>
--- mvc/app/views/inc/header.php
+++ mvc/app/views/inc/header.php
@@ -3,6 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title><?= SITENAME; ?></title>
+    <link rel="stylesheet" href="<?= URLROOT ?>/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= URLROOT ?>/css/all.min.css">
     <link rel="stylesheet" href="<?= URLROOT ?>/css/styles.css">
   </head>
   <body>
+    <?php
+      require_once APPROOT ."/views/inc/navbar.php";
+    ?>
+    <main role="main" class="container">

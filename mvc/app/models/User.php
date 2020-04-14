239b941da140f05c4b41f5c1e8ad2409ac6a8e0b
Simon Taylor <simon.taylor@tyxan.com>
2020-04-14 12:57:57 +0100

    Prepare and fire registration form input. Return true/false

--- mvc/app/models/User.php
+++ mvc/app/models/User.php
@@ -10,10 +10,26 @@
         $this->db = new Database;
       }
 
+      // Register user account
+      public function register($data) {
+        $this->db->Query("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
+        // Bind values
+        $this->db->bind(":name", $data['name']);
+        $this->db->bind(":email", $data['email']);
+        $this->db->bind(":password", $data['password']);
+        // Execute query
+        if($this->db->execute()) :
+          return true;
+        else :
+          return false;
+        endif;
+      }
+
       // Find user by email
       public function findUserByEmail($email) {
         // Prepare query and bind info
         $this->db->query("SELECT * FROM users WHERE email = :email");
+        // Bind values
         $this->db->bind(":email", $email);
         // Grab single result
         $this->db->single();

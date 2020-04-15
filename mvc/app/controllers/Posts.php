8a1c267bee8d9b97cf91410d1d5fa6221d681885
Simon Taylor <simon.taylor@tyxan.com>
2020-04-15 10:41:09 +0100

    Add and show methods added

--- mvc/app/controllers/Posts.php
+++ mvc/app/controllers/Posts.php
@@ -20,14 +20,62 @@
       $this->view('posts/index', $data);
     }
 
+    public function show($id) {
+      $data = [
+
+      ];
+      $this->view('posts/show', $data);
+    }
+
     public function add() {
 
-    $data = [
-      'title' => '',
-      'body' => ''
-    ];
+      if($_SERVER['REQUEST_METHOD'] == "POST") :
 
-    $this->view('posts/add', $data);
+        // Sanitize the post
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+        $data = [
+          'title' => trim($_POST['title']),
+          'body' => trim($_POST['body']),
+          'user_id' => $_SESSION['user_id'],
+          'title_error' => "",
+          'body_error' => ""
+        ];
+
+        // Validate title
+        if(empty($data['title'])) :
+          $data['title_error'] = "Please enter title";
+        endif;
+
+        // Validate body
+        if(empty($data['body'])) :
+          $data['body_error'] = "Please enter body";
+        endif;
+
+        // Check for errors
+        if(empty($data['title_error']) && empty($data['body_error'])) :
+          // Validated
+          if($this->postModel->addPost($data)) :
+            flash("post_message", "Post added");
+            redirect("posts");
+          else :
+            die("Simoething went wrong");
+          endif;
+        else:
+          // Load view with errors
+          $this->view('posts/add', $data);
+        endif;
+
+      else :
+
+        $data = [
+          'title' => '',
+          'body' => ''
+        ];
+
+        $this->view('posts/add', $data);
+
+      endif;
 
     }
 
